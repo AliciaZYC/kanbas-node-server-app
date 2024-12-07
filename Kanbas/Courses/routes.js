7362ee78a50f11aafc72b82ed799f08df9c0177e
Yichi Zhang <yichizhang@Yichis-MacBook-Pro.local>
2024-12-06 17:57:22 -0800

    small changes

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -63,7 +63,7 @@ export default function CourseRoutes(app) {
       // availableFrom: req.body.availableFrom || "2024-05-06T23:59",
       // availableUntil: req.body.availableUntil || "2024-05-20T23:59",
     };
-    const newAssignment = assignmentsDao.createAssignment(assignment);
+    const newAssignment = await assignmentsDao.createAssignment(assignment);
     res.send(newAssignment);
   });
 }
